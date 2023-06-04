4abe2c4b14b236e739cb5bbe3a7fc25597038ec0
Upsidedrop <96622429+Upsidedrop@users.noreply.github.com>
2023-06-03 23:31:58 -0400

    Update 1.03/IHATEMYLIFE
    This script is a MonoBehaviour class in Unity that allows the user to save an image represented by a Tilemap. The image is converted into a boolean array and saved as JSON data.
    
    The Save class contains the main functionality. It has a reference to a Tilemap and a TileBase representing an empty tile. The script listens for the "Return" key press and triggers the SaveCurrentImage method.
    
    The SaveCurrentImage method creates a boolean array (imgBool) to store the image data. It iterates over the Tilemap, checking if each tile is empty or not. Based on the result, it sets the corresponding value in the imgBool array. It also creates an instance of the Image class, populates its imgBool array with the image data, and sets a correct output value.
    
    The method then checks if a JSON save file already exists. If it does, it reads the file and deserializes its contents into a list of Image objects using the JsonUtility.FromJson method. Next, it adds the new Image instance to the list.
    
    The list is wrapped in a JsonableListWrapper class to ensure compatibility with the JsonUtility methods. After wrapping the list, the updated list is serialized back into JSON using JsonUtility.ToJson, and the JSON data is written to a save file.
    
    The script also includes the Image class, which represents an image with a boolean array (imgBool) and a correct output value. Additionally, there is a JsonableListWrapper class that acts as a wrapper for generic lists, enabling easy serialization and deserialization of lists using JsonUtility.
    
    The script demonstrates a simple way to save image data as JSON using Unity's JsonUtility and File I/O operations.

--- NumberDetector/Assets/Scripts/Save.cs
+++ NumberDetector/Assets/Scripts/Save.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -7,7 +11,7 @@ public class Save : MonoBehaviour
     Tilemap tilemap;
     [SerializeField]
     TileBase emptyTile;
-    bool[] image;
+    bool[] imgBool;
 
     private void Update()
     {
@@ -18,43 +22,70 @@ public class Save : MonoBehaviour
     }
     void SaveCurrentImage()
     {
-        image = new bool[784];
+        imgBool = new bool[784];
         for (int i = 0; i < 28; i++)
         {
             for (int i1 = 0; i1 < 28; i1++)
             {
                 if (tilemap.GetTile(new(i - 14, i1 - 14)) == emptyTile)
                 {
-                    image[i * 28 + i1] = false;
+                    imgBool[i * 28 + i1] = false;
                 }
                 else
                 {
-                    image[i * 28 + i1] = true;
+                    imgBool[i * 28 + i1] = true;
                 }
             }
         }
-        PlayerPrefs.SetInt("ImageNumber", PlayerPrefs.GetInt("ImageNumber") + 1);
-        PlayerPrefs.SetString(
-            "EncodedImage" + PlayerPrefs.GetInt("ImageNumber"),
-            "" + ConvertBoolArrToBinary(image));
-        print(ConvertBoolArrToBinary(image));
-    }
-    //probably copied from stackoverflow
-    string ConvertBoolArrToBinary(bool[] boolArr)
-    {
-        string result = "";
-        for (int i = 0; i < boolArr.Length; i++)
-        {
-            if (boolArr[i])
-            {
-                result += "1";
-            }
-            else
-            {
-                result += "0";
-            }
-        }
-        return result;
-    }
+        Image image = new Image();
 
+        List<Image> tempImageList = new List<Image>();
+
+        for (int i = 0; i < imgBool.Length; i++)
+        {
+            image.imgBool[i] = imgBool[i];
+
+        }
+
+        image.correctOutput = 42;
+        // Load existing JSON data into a list
+        if (File.Exists(Application.dataPath + "/saveFile.json"))
+        {
+            string json = File.ReadAllText(Application.dataPath + "/saveFile.json");
+            tempImageList = JsonUtility.FromJson<JsonableListWrapper<Image>>(json).list;
+
+        }
+        // Add new data to the list
+        print(tempImageList == null);        
+        tempImageList.Add(image);
+
+        JsonableListWrapper<Image> wrappedList = new(tempImageList);
+        // Save the updated list back to the JSON file
+        string listAsJson = JsonUtility.ToJson(wrappedList);
+        print(listAsJson);
+        File.WriteAllText(Application.dataPath + "/saveFile.json", listAsJson);
+    }
+    [Serializable]
+    private class Image
+    {
+        public bool[] imgBool = new bool[784];
+        public int correctOutput;
+    }
+}
+[Serializable]
+public class JsonableListWrapper<T>
+{
+    public List<T> list;
+    public JsonableListWrapper(List<T> list)
+    {
+        this.list = list;
+    }
+    /*
+    //For future reference
+    List<string> stringList = new List<string>(){"one","two","three"};
+    // To Json
+    string stringListAsJson = JsonUtility.ToJson(new JsonableListWrapper<string>(stringList));
+    // From Json
+    List<string> stringListFromJson = JsonUtility.FromJson<JsonableListWrapper<string>>(stringListAsJson).list;
+    */
 }
